c976af7e88fd1e252b0c1378aae7b7e4d388e4e4
ne_Sachirou <utakata.c4se@gmail.com>
2013-02-10 01:29:54 +0900

    NuSpec for NuGet Gallery.

--- LTSV/Properties/AssemblyInfo.cs
+++ LTSV/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("LTSV")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("LTSV.NET")]
+[assembly: AssemblyDescription("C# implementation of LTSV (Labeled Tab-separated Values).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LTSV")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Public Domain  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
